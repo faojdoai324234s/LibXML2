74062c1170623a714cfeb191df8d46a337943a5c
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-03-03 17:21:34 +0000

    Create build_win.bat

---
+++ build_win.bat
@@ -0,0 +1,56 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set LIBXMLVERSION = "libxml2-2.13.6"
+set PROGFILES=%ProgramFiles%
+if not "%ProgramFiles(x86)%" == "" set PROGFILES=%ProgramFiles(x86)%
+
+REM Check if Visual Studio 2019 is installed
+set MSVCDIR="%PROGFILES%\Microsoft Visual Studio\2019"
+set VCVARSALLPATH="%PROGFILES%\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
+if exist %MSVCDIR% (
+  if exist %VCVARSALLPATH% (
+   	set COMPILER_VER="2019"
+   	set VCVERSION = 16
+   	echo Using Visual Studio 2019 Enterprise
+	goto begin
+  )
+)
+
+echo No compiler found : Microsoft Visual Studio 2019 Enterprise is not installed.
+goto end
+
+:begin
+
+REM Download latest libXML2 and rename to libxml.zip
+echo Downloading curl...
+powershell -command "(new-object System.Net.WebClient).DownloadFile('https://download.gnome.org/sources/libxml2/2.13/libxml2-2.13.6.tar.xz')"
+
+REM Extract downloaded zip file to tmp_libcurl
+REM "C:\Program Files\7-Zip\7z.exe" x libxml.zip -y -otmp_libxml
+REM del libxml.zip
+
+REM cd tmp_libxml
+
+REM Build!
+echo "Building libxml now!"
+
+if [%1]==[-static] (
+	set RTLIBCFG=static
+	echo Using /MT instead of /MD
+) 
+
+echo "Path to vcvarsall.bat: %VCVARSALLPATH%"
+call %VCVARSALLPATH% x64
+
+cmake -E tar xf %LIBXMLVERSION%.tar.xz
+cmake -S %LIBXMLVERSION% -B %LIBXMLVERSION%-build BUILD_SHARED_LIBS=ON CMAKE_BUILD_TYPE=Debug LIBXML2_WITH_ICONV=OFF LIBXML2_WITH_PYTHON=OFF
+cmake --build %LIBXMLVERSION%-build
+cmake --install %LIBXMLVERSION%-build
+
+cd D:\a\LibXML2\LibXML2\%LIBXMLVERSION%\build
+dir /s
+
+:end
+echo Done.
+exit /b
