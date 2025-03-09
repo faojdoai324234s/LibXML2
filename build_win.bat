71669138c52bb4ac61a76c934d110d7d4ad6ff14
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-03-09 17:55:24 +0000

    Update build_win.bat

--- build_win.bat
+++ build_win.bat
@@ -8,7 +8,7 @@ mkdir upload\Release
 
 REM Download latest libXML2
 git clone https://github.com/GNOME/libxml2
-dir
+
 REM Build Debug configuration
 cmake -S libxml2 -B build -D LIBXML2_WITH_ICONV=OFF -D LIBXML2_WITH_PYTHON=OFF
 cmake --build build --config Debug
@@ -16,8 +16,8 @@ cmake --install build
 
 REM Copy over the built files
 copy /y /v build\Debug\*.dll upload\Debug
-copy /y /v build\Debug\libxml2.lib upload\Debug\libxml2_debug.lib
-copy /y /v build\Debug\*.pdb upload\Debug
+copy /y /v build\Debug\*.lib upload\Debug
+copy /y /v build\Debug\libxml2d.pdb upload\Debug
 
 REM Clean up before we run CMake again
 rmdir /s /q build
@@ -32,6 +32,6 @@ copy /y /v build\Release\*.dll upload\Release
 copy /y /v build\Release\*.lib upload\Release
 
 REM Copy over the headers
-copy /y /v libxml2\include upload\include\libxml2
+xcopy /y /v /s /e libxml2\include upload\include\libxml2
 
 exit /b
