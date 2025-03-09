cb2a44290e8c12a62d342e47621fb6cbfd62b4dd
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-03-09 18:32:02 +0000

    Create build_linux.sh

---
+++ build_linux.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Prepare the build with creating the file structure
+mkdir upload
+mkdir upload/include
+mkdir upload/include/libxml2
+mkdir upload/Debug
+mkdir upload/Release
+
+git clone https://github.com/GNOME/libxml2
+
+# Build Debug configuration
+cmake -S libxml2 -B build -D LIBXML2_WITH_ICONV=OFF -D LIBXML2_WITH_PYTHON=OFF -D CMAKE_BUILD_TYPE=Debug
+cmake --build build --config Debug
+cmake --install build
+
+# Copy over the built files
+cp build/libxml2.so upload/Debug/libxml2d.so
+
+# Clean up before we run CMake again
+rm -rf build
+
+# Build Release configuration
+cmake -S libxml2 -B build -D LIBXML2_WITH_ICONV=OFF -D LIBXML2_WITH_PYTHON=OFF -D CMAKE_BUILD_TYPE=Release
+cmake --build build --config Release
+cmake --install build
+
+# Copy over the built files
+cp build/libxml2.so upload/Release
+
+# Copy over the headers
+cp -r libxml2/include/. upload/include/libxml2
